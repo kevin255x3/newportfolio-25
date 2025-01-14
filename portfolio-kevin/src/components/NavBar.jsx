344122b323b93eb8291780a67f9a7488be60e0f7
kevin255x3 <hkevinlazo@gmail.com>
2025-01-13 17:57:11 -0800

    Update NavBar.jsx

--- portfolio-kevin/src/components/NavBar.jsx
+++ portfolio-kevin/src/components/NavBar.jsx
@@ -1,28 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
-        <nav className="  py-2">
+        <nav className="py-2">
             <div className="container mx-auto flex items-center justify-between px-8">
                 {/* Logo Section */}
                 <div className="flex items-center">
                     <img src="./logo.png" alt="Logo" className="w-36 h-16 object-contain" />
-                    {/* <p className="text-black font-artbold font-bold text-lg tracking-wide">KEVIN LAZO ++</p> */}
                 </div>
 
                 {/* Navigation Links */}
                 <ul className="flex space-x-6">
                     <li>
-                        <a href="/" className="text-black hover:text-royal font-ming  text-sm">Home</a>
+                        <Link to="/" className="text-black hover:text-royal font-ming text-sm">
+                            Home
+                        </Link>
                     </li>
                     <li>
-                        <a href="/about" className="text-black hover:text-royal font-ming  text-sm">About</a>
+                        <Link to="/about" className="text-black hover:text-royal font-ming text-sm">
+                            About
+                        </Link>
                     </li>
                     <li>
-                        <a href="/projects" className="text-black hover:text-royal font-ming  text-sm">Projects</a>
+                        <Link to="/projects" className="text-black hover:text-royal font-ming text-sm">
+                            Projects
+                        </Link>
                     </li>
                     <li>
-                        <a href="/contact" className="text-black hover:text-royal font-ming  text-sm">Contact</a>
+                        <Link to="/contact" className="text-black hover:text-royal font-ming text-sm">
+                            Contact
+                        </Link>
                     </li>
                 </ul>
             </div>
